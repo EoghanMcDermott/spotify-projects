da38d17e4ca11790994816f3d080ca82b740486c
Eoghan McDermott <eoghan.mc-dermott.1@ucdconnect.ie>
2020-11-12 12:45:20 +0000

    removed redundant comments

--- print_playlists.py
+++ print_playlists.py
@@ -3,12 +3,10 @@ from spotipy.oauth2 import SpotifyClientCredentials
 
 import config
 
-# don't forget to set env variables SPOTIPY_CLIENT_ID and SPOTIPY_CLIENT_SECRET
 auth_manager = SpotifyClientCredentials(config.client_id, config.client_secret)
 
 sp = spotipy.Spotify(auth_manager=auth_manager)
 
-# user_id can be found in the settings of your account
 playlists = sp.user_playlists(config.username)
 while playlists:
     for i, playlist in enumerate(playlists['items']):
